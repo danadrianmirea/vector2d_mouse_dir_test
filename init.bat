ca4971c79ff44d6acb614270fea26918b9fdcfcd
Dan Adrian Mirea <damirea84@gmail.com>
2024-08-30 23:45:12 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "vector2d_mouse_dir_test" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/vector2d_mouse_dir_test.git
-git push -u origin main
